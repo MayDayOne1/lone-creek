57a2c756e2b5520dfa6a3972cbc407a3e81ce0cf
pmilowski <pmilowski16@gmail.com>
2023-04-27 22:28:49 +0200

    Pre-throwable
    Bottle prefab created
    ItemPickup update
    InputSystem update

--- Assets/Script/ItemPickup.cs
+++ Assets/Script/ItemPickup.cs
@@ -1,21 +1,15 @@
 using UnityEngine;
+using Zenject;
 
 public class ItemPickup : MonoBehaviour
 {
-    public float pickupRadius = 1f; // promień, w którym gracz może podnieść przedmiot
-    public LayerMask playerLayer; // warstwa, na której znajduje się gracz
-    public string playerTag = "Player"; // tag, którym oznaczony jest gracz
+    [SerializeField] private PlayerInteract playerInteract;
 
-    private void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, pickupRadius, playerLayer); // pobierz kolizje w promieniu pickupRadius z warstwą playerLayer
-
-        foreach (Collider collider in colliders)
+        if(other.tag == "Player")
         {
-            if (collider.CompareTag(playerTag)) // sprawdź, czy tag obiektu jest taki sam jak playerTag
-            {
-                Debug.Log("Gracz jest w promieniu przedmiotu, który może podnieść."); // jeśli tak, wyświetl komunikat w konsoli
-            }
+           if(playerInteract.)
         }
     }
 }
