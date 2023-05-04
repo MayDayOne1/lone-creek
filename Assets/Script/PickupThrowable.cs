4db50bdcf6dd95b27c2107cd078f37118fa947a8
pmilowski <pmilowski16@gmail.com>
2023-05-04 21:38:52 +0200

    Item destroyed when E pressed
    choose interactable item based on distance

--- Assets/Script/PickupThrowable.cs
+++ Assets/Script/PickupThrowable.cs
@@ -7,5 +7,6 @@ public class PickupThrowable : MonoBehaviour
     public void Interact()
     {
         Debug.Log("I am interacted!");
+        Destroy(gameObject);
     }   
 }
--- Assets/Script/PlayerInteract.cs
+++ Assets/Script/PlayerInteract.cs
@@ -1,24 +1,59 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerInteract : MonoBehaviour
 { 
-    private Collider OnTriggerEnter(Collider other)
+    List<Collider> objectsTriggered = new List<Collider>();
+    public GameObject Throwable;
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Throwable")
         {
-            Debug.Log("I'm throwable!");
-            other.GetComponent<PickupThrowable>().Interact();
+            // Debug.Log("I'm throwable!");
         }
-        Collider result = other;
-        return result;
+        objectsTriggered.Add(other);
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objectsTriggered.Remove(other);
+    }
+
+    private Collider ChooseInteractiveObject()
+    {
+        float dist;
+        float minDist = float.MaxValue;
+        int resultingIndex = 0;
+        // Debug.Log("interact");
+        if (objectsTriggered.Count > 0)
+        {
+            for (int i = 0; i < objectsTriggered.Count; i++)
+            {
+                dist = Vector3.Distance(objectsTriggered[i].transform.position, transform.position);
+                if (dist < minDist)
+                {
+                    resultingIndex = i;
+                    minDist = dist;
+                }
+            }
+            return objectsTriggered[resultingIndex];
+        } else
+        {
+            return null;
+        }
+    }
+
     public void Interact()
     { 
-        Debug.Log("interact");
+        // TO DO: choose component to get by tag
+
+        if(objectsTriggered.Count > 0)
+        {
+            Collider obj = ChooseInteractiveObject();
+            obj.GetComponent<PickupThrowable>().Interact();
+            objectsTriggered.Remove(obj);
+        }
     }
 
     
