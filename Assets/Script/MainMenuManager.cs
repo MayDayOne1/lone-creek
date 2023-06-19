f13629dc4c29f84a385f58004590d10d7de76bb7
pmilowski <pmilowski16@gmail.com>
2023-06-19 04:19:24 +0200

    trash commit

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -20,7 +20,7 @@ public class MainMenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("SceneTunnel");
+        SceneManager.LoadScene("Tunnel");
     }
 
     public void QuitGame()
