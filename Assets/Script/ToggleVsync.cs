403d8df0f0cc1f082fe5952ed1f52152e71880ef
pmilowski <pmilowski16@gmail.com>
2023-11-24 13:44:10 +0100

    Update ToggleVsync.cs

--- Assets/Script/ToggleVsync.cs
+++ Assets/Script/ToggleVsync.cs
@@ -7,16 +7,12 @@ public class ToggleVsync : MonoBehaviour
 
     private void Start()
     {
-        // Pobierz komponent Toggle w skrypcie
         toggle = GetComponent<Toggle>();
 
-        // SprawdŸ, czy komponent Toggle zosta³ poprawnie znaleziony
         if (toggle != null)
         {
-            // Ustaw stan Toggle na obecny stan Vsync
             toggle.isOn = QualitySettings.vSyncCount > 0;
 
-            // Dodaj obiekt nas³uchuj¹cy na zmianê stanu Toggle
             toggle.onValueChanged.AddListener(ToggleVsyncCallback);
         }
         else
@@ -27,10 +23,7 @@ public class ToggleVsync : MonoBehaviour
 
     private void ToggleVsyncCallback(bool isVsyncEnabled)
     {
-        // Ustaw Vsync w zale¿noœci od stanu Toggle
         QualitySettings.vSyncCount = isVsyncEnabled ? 1 : 0;
-
-        // Wyœwietl informacjê w konsoli po zmianie stanu Vsync
         Debug.Log("Vsync is now " + (QualitySettings.vSyncCount > 0 ? "enabled" : "disabled"));
     }
 }
