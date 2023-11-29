1ccce70525bb07111748ae3411f25090e1a53917
pmilowski <pmilowski16@gmail.com>
2023-11-29 12:55:12 +0100

    enemy footstep sound

--- Assets/Script/Enemy/EnemyFootstepManager.cs
+++ Assets/Script/Enemy/EnemyFootstepManager.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class EnemyFootstepManager : MonoBehaviour
 {
+    [SerializeField] Transform footstepEmitter;
+    [SerializeField] AudioClip[] footsteps;
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private AudioClip SelectRandomFootstep()
+    {
+        return footsteps[Random.Range(0, footsteps.Length)];
     }
 
     private void Step()
     {
-        Debug.Log("Step!");
+        AudioClip clip = SelectRandomFootstep();
+        AudioSource.PlayClipAtPoint(clip, footstepEmitter.position);
     }
 }
