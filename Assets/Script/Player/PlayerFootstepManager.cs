95762daa0b2c9cde4563ce6cd618a847c4e510c3
pmilowski <pmilowski16@gmail.com>
2023-11-29 13:34:02 +0100

    Player footstep sound

--- Assets/Script/Player/PlayerFootstepManager.cs
+++ Assets/Script/Player/PlayerFootstepManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFootstepManager : MonoBehaviour
+public class PlayerFootstepManager : MonoBehaviour
 {
     [SerializeField] Transform footstepEmitter;
     [SerializeField] AudioClip[] footsteps;
@@ -12,9 +12,7 @@ public class EnemyFootstepManager : MonoBehaviour
         return footsteps[Random.Range(0, footsteps.Length)];
     }
 
-#pragma warning disable IDE0051 // Remove unused private members
     private void Step()
-#pragma warning restore IDE0051
     {
         AudioClip clip = SelectRandomFootstep();
         AudioSource.PlayClipAtPoint(clip, footstepEmitter.position);
